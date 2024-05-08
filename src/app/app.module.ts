27616bce38722e8eb3163ae9769946f39fef3455
Alexandru Muresan <muresan.alexandru933@gmail.com>
2024-05-08 09:26:55 +0300

    Project upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AComponent } from './components/a/a.component';
+import { BComponent } from './components/b/b.component';
+import { CComponent } from './components/c/c.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AComponent,
+    BComponent,
+    CComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
